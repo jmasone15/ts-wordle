bd349adb4477fb6b60908ad4684b369314d054bf
Jordan Masone <jordanmasone1@gmail.com>
2023-05-23 13:42:41 -0400

    Added words.json and imported

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,2 +1,9 @@
 "use strict";
-console.log("test");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const words_json_1 = __importDefault(require("./utils/words.json"));
+const randomWord = () => {
+    return words_json_1.default[Math.floor(Math.random() * words_json_1.default.length)];
+};
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,20 @@
-console.log("test");
+// TODO
+// 1. Randomly select a word from the JSON file.
+// 2. Open the current guess row for input
+// 3. When typing the fifth letter, check to see if the word is a real word.
+// 4. If the word is a real word, check each letter to see if it's in the word or in the right place
+// 5. Run animation on each letter and update class.
+// 6. Update the keyboard button classes.
+// 7. If the user has anymore guesses, start back at the beginning
+// 8. If the user correctly guessed the word, game over!
+
+// Post MVP
+// 1. Light/Dark Mode
+// 2. Statistics
+// 3. Help
+// 4. Word History
+import wordJson from "./utils/words.json";
+
+const randomWord = (): string => {
+  return wordJson[Math.floor(Math.random() * wordJson.length)];
+};
