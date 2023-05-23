481718a20b414e02318b8977f1356172035647c4
Jordan Masone <jordan.masone@gfs.com>
2023-05-23 19:37:12 -0400

    Refactored starting code

--- src/script.ts
+++ src/script.ts
@@ -1,7 +1,7 @@
 // TODO
 // 1. Randomly select a word from the JSON file.
 // 2. Open the current guess row for input
-// 3. When typing the fifth letter, check to see if the word is a real word.
+// 3. When hitting enter, check to see if the word is a real word.
 // 4. If the word is a real word, check each letter to see if it's in the word or in the right place
 // 5. Run animation on each letter and update class.
 // 6. Update the keyboard button classes.
@@ -13,86 +13,86 @@
 // 2. Statistics
 // 3. Help
 // 4. Word History
-let answerArray: string[] = [];
+
+// Game Variables
+let answerArray: string[];
 let targetWord: string;
-const keyboardBtns = [...document.getElementsByTagName("button")];
+let guessNum: number;
+let userInput = false;
 
-const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
-keyboardBtns.forEach((element) => {
-    element.addEventListener("click", async (event) => {
-        const button = event.target as HTMLElement;
-
-        if ((button.getAttribute("id") === "backspace" || button.getAttribute("id") === "backspace-icon") && answerArray.length > 0) {
-            const targetRow = document.getElementById("row1") as HTMLElement;
-            const targetBox = targetRow.children[answerArray.length - 1] as HTMLElement;
-            targetBox.innerHTML = "";
-            answerArray.pop();
-        } else if (button.innerText === "ENTER") {
-            if (answerArray.length === 5) {
-                const targetRow = document.getElementById("row1") as HTMLElement;
-
-                for (let i = 0; i < answerArray.length; i++) {
-                    const targetBox = targetRow.children[i] as HTMLElement;
-                    if (answerArray[i] === targetWord[i]) {
-                        targetBox.classList.add("correct");
-                    } else if (targetWord.includes(answerArray[i])) {
-                        targetBox.classList.add("partial");
-                    } else {
-                        targetBox.classList.add("incorrect");
-                    }
-
-                    targetBox.setAttribute("style", "animation: flip-in 0.5s");
-                    await delay(500);
-                }
-            }
-        } else if (answerArray.length < 5) {
-            answerArray.push(button.innerText.toLowerCase());
-            const targetRow = document.getElementById("row1") as HTMLElement;
-            const targetBox = targetRow.children[answerArray.length - 1] as HTMLElement;
-            targetBox.innerHTML = `<p>${button.innerText}</p>`;
-        }
-    });
-});
-
-document.addEventListener("keydown", async (event) => {
-    if (event.key.charCodeAt(0) > 96 && event.key.charCodeAt(0) < 123 && answerArray.length < 5) {
-        answerArray.push(event.key);
-        const targetRow = document.getElementById("row1") as HTMLElement;
-        const targetBox = targetRow.children[answerArray.length - 1] as HTMLElement;
-        targetBox.innerHTML = `<p>${event.key}</p>`;
-    } else if (event.key === "Backspace" && answerArray.length > 0) {
-        const targetRow = document.getElementById("row1") as HTMLElement;
-        const targetBox = targetRow.children[answerArray.length - 1] as HTMLElement;
-        targetBox.innerHTML = "";
+// Game Functions
+const gameStart = async (): Promise<void> => {
+    // Game Variables
+    answerArray = [];
+    guessNum = 1;
+    userInput = true;
+    await randomWord();
+};
+const getLetterBox = (num?: number): HTMLElement => {
+    const targetRow = document.getElementById(`row${guessNum}`) as HTMLElement;
+    return targetRow.children[num === undefined ? answerArray.length - 1 : num] as HTMLElement;
+};
+const deleteLetter = (): void => {
+    if (answerArray.length > 0) {
+        getLetterBox().innerHTML = "";
         answerArray.pop();
-    } else if (event.key === "Enter") {
-        if (answerArray.length === 5) {
-            const targetRow = document.getElementById("row1") as HTMLElement;
+    }
+};
+const typeLetter = (letter: string): void => {
+    if (answerArray.length < 5) {
+        answerArray.push(letter.toLowerCase());
+        getLetterBox().innerHTML = `<p>${letter}</p>`;
+    }
+};
+const submitWord = async (): Promise<void> => {
+    if (answerArray.length === 5) {
+        for (let i = 0; i < answerArray.length; i++) {
+            let targetBox = getLetterBox(i);
+            const letter = answerArray[i];
 
-            for (let i = 0; i < answerArray.length; i++) {
-                const targetBox = targetRow.children[i] as HTMLElement;
-                if (answerArray[i] === targetWord[i]) {
-                    targetBox.classList.add("correct");
-                } else if (targetWord.includes(answerArray[i])) {
-                    targetBox.classList.add("partial");
-                } else {
-                    targetBox.classList.add("incorrect");
-                }
-
-                targetBox.setAttribute("style", "animation: flip-in 0.5s");
-                await delay(500);
+            if (letter === targetWord[i]) {
+                targetBox.classList.add("correct");
+            } else if (targetWord.includes(letter)) {
+                targetBox.classList.add("partial");
+            } else {
+                targetBox.classList.add("incorrect");
             }
+
+            targetBox.setAttribute("style", "animation: flip-in 0.5s");
+            await delay(500);
+            targetBox.removeAttribute("style");
         }
     }
-});
-
+};
 const randomWord = async (): Promise<void> => {
     const response = await fetch("./assets/utils/words.json");
     const data = await response.json();
+    const randomIdx = Math.floor(Math.random() * data.length);
 
-    targetWord = data[Math.floor(Math.random() * data.length)].toLowerCase();
-    console.log(targetWord);
+    targetWord = data[randomIdx].toLowerCase();
 };
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
-randomWord();
+// Event Listeners
+[...document.getElementsByTagName("button")].forEach(element => {
+    element.addEventListener("click", () => {
+        if (["backspace", "backspace-icon"].includes(element.getAttribute("id") as string)) {
+            return deleteLetter();
+        } else if (element.innerText === "ENTER") {
+            return submitWord();
+        } else {
+            return typeLetter(element.innerText);
+        }
+    });
+});
+document.addEventListener("keydown", ({ key }) => {
+    if (key.charCodeAt(0) > 96 && key.charCodeAt(0) < 123) {
+        return typeLetter(key);
+    } else if (key === "Backspace") {
+        return deleteLetter();
+    } else if (key === "Enter") {
+        return submitWord();
+    }
+});
+
+gameStart();
